f976223db38ba241a5246386d85a3709a862f014
zjz123456 <18074533267@163.com>
2018-03-23 12:36:33 +0800

    back

--- app/src/main/java/com/sy/shoplist/shoplist/activity/BaseActivity.java
+++ app/src/main/java/com/sy/shoplist/shoplist/activity/BaseActivity.java
@@ -3,6 +3,7 @@ package com.sy.shoplist.shoplist.activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import static com.sy.shoplist.shoplist.control.ActivityCollector.addActivity;
 import static com.sy.shoplist.shoplist.control.ActivityCollector.removeActivity;
@@ -12,6 +13,7 @@ import static com.sy.shoplist.shoplist.control.ActivityCollector.removeActivity;
  */
 
 public class BaseActivity extends AppCompatActivity {
+    private long mExitTime = System.currentTimeMillis();  //为当前系统时间，单位：毫秒
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
@@ -22,5 +24,15 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         removeActivity(this);
+    }
+    @Override
+    public void onBackPressed() {
+        if(System.currentTimeMillis() - mExitTime < 800) {
+            this.finish();   //关闭本活动页面
+        }
+        else{
+            Toast.makeText(this,"再按返回键退出！",Toast.LENGTH_SHORT).show();
+            mExitTime = System.currentTimeMillis();   //这里赋值最关键，别忘记
+        }
     }
 }
